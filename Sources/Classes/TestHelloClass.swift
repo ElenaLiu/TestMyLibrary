7d3bab80b4b193405123316ad248448723a2e0ed
elena.liu <elena.liu@baifu-tech.net>
2024-04-28 12:42:22 +0800

    first commit

--- Sources/Classes/TestHelloClass.swift
+++ Sources/Classes/TestHelloClass.swift
@@ -8,7 +8,22 @@
 import Foundation
 
 @objc public class TestHelloClass: NSObject {
-    public static func hello() {
+    @objc public let id: Int
+    @objc public let title: String
+    @objc public func getTitle() -> String { title }
+    @objc public func printTitle() {
+        print("title: \(title)")
+    }
+    @objc public init(id: Int, title: String) {
+        self.id = id
+        self.title = title
+    }
+    
+    @objc public static func hello() {
+        print("TestHelloClass hello!")
+    }
+    
+    @objc public func hello2() {
         print("TestHelloClass hello!")
     }
 }
