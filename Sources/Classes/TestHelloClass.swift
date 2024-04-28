40145ea6bafe40d8ce214b073ca9098630bfc5c4
elena.liu <elena.liu@baifu-tech.net>
2024-04-28 12:14:37 +0800

    first commit

---
+++ Sources/Classes/TestHelloClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestHelloClass.swift
+//  TestProject
+//
+//  Created by Elena on 2024/4/28.
+//
+
+import Foundation
+
+@objc public class TestHelloClass: NSObject {
+    public static func hello() {
+        print("TestHelloClass hello!")
+    }
+}
